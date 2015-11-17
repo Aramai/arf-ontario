63d1e0b356839cb5fc4571510e61fd7a79fd4ce4
Sean Clark <sean@sean-clark.com>
2015-11-17 16:17:54 -0500

    Added showings content & script, fixed json/image links

--- arf-ontario/App/adoptions/adoptions.controller.js
+++ arf-ontario/App/adoptions/adoptions.controller.js
@@ -10,12 +10,16 @@
         vm.title = 'Adoptions';
         vm.dogs = undefined;
         vm.cats = undefined;
+        vm.showings = undefined;
         
         if ($state.is('adoptions.dogs') && vm.dogs == undefined) {
-            $http.get('./App/adoptions/adoptableDogs.json').success(GetAdoptableDogs);
+            $http.get('App/adoptions/adoptableDogs.json').success(GetAdoptableDogs).error(ReportError);
         }
         else if ($state.is('adoptions.cats') && vm.cats == undefined) {
-            $http.get('./App/adoptions/adoptableCats.json').success(GetAdoptableCats);
+            $http.get('App/adoptions/adoptableCats.json').success(GetAdoptableCats).error(ReportError);
+        }
+        else if ($state.is('adoptions.showings') && vm.showings == undefined) {
+            $http.get('App/adoptions/showings.json').success(GetAdoptionShowings).error(ReportError);
         }
         
         function GetAdoptableDogs(response) {
@@ -29,6 +33,11 @@
 
             ReplaceNewLines(vm.cats);
         }
+
+        function GetAdoptionShowings(response) {
+            vm.showings = response.showings;
+        }
+
 
         function ReportError(response) {
             console.log(response);
