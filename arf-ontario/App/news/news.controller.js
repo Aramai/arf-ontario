2c012812a5bf77c52d2c44b0fb33ca9dbc109023
Sean Clark <sean@sean-clark.com>
2015-12-21 15:57:13 -0500

    Added news

--- arf-ontario/App/news/news.controller.js
+++ arf-ontario/App/news/news.controller.js
@@ -5,8 +5,21 @@
         .module('app.news')
         .controller('News', News);
 
-    function News() {
+    function News($http, $state) {
         var vm = this;
         vm.title = 'News';
+        vm.items = undefined;
+
+        if ($state.is('news') && vm.items == undefined) {
+            $http.get('App/news/news.json').success(getNewsItems).error(reportError);
+        }
+
+        function getNewsItems(response) {
+            vm.items = response.items            
+        }
+
+        function reportError(response) {
+            console.log(response);
+        }
     }
 })();
